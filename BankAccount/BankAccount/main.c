ebc6ad28f5c6a7aa51a3aabf1a128fabbbed04f8
Choiyu330 <jenny4063@naver.com>
2020-02-05 14:16:03 +0900

    기초연산자

--- BankAccount/BankAccount/main.c
+++ BankAccount/BankAccount/main.c
@@ -1,6 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int main() {
+	
+	int num,num_2;
 
-main() {
-	printf("Hello world!");
+	printf("두 개의 정수입력 : "); scanf("%d %d", &num, &num_2);
+	printf("%d가 %d보다 큰가?\n", num, num_2);
+	//삼항 연산자
+	printf("(1 = 예, 0 = 아니오)\n\n답 : %d \n", num > num_2 ? 1 : 0); 
+
+	
+	return 0;
 }
